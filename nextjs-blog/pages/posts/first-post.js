675016048b680e76d6a726c778afa2fe15482d0b
AzinYad <azin.yadegari@gmail.com>
2023-06-03 12:57:57 +0100

    adding img

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function FirstPost() {
     return (
         <div>
             <h1>First Post</h1>
             <h2>
+                <Image
+                    src="/images/profile.jpg" // Route of the image file
+                    height={144} // Desired size with correct aspect ratio
+                    width={144} // Desired size with correct aspect ratio
+                    alt="Your Name"
+                /><br></br>
                 <Link href="/">Back to home</Link>
             </h2>
         </div>
