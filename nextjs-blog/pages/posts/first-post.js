0646f5ce36a2ae6a87b90ddf7194844245ff4a5c
AzinYad <azin.yadegari@gmail.com>
2023-06-06 17:01:37 +0100

    adding head title

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 
 export default function FirstPost() {
     return (
         <div>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Image
